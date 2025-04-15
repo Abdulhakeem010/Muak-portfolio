a85435a80f6005b045a176b5674b51b8a93e5b4f
abdulhakeem010 <bigmuak010@gmail.com>
2025-04-15 11:22:58 +0100

    Add JavaScript functionality to hide header on scroll and link resume.js in resume.html

---
+++ resume/resume.js
@@ -0,0 +1,16 @@
+let lastScrollTop = 0; 
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.scrollY || document.documentElement.scrollTop; 
+
+  if (currentScroll > lastScrollTop) {
+  
+    header.classList.add("hidden");
+  } else {
+  
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
