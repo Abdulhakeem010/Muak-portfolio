421fdf2a3cd20e60c86a265201e2cf6209a0628e
abdulhakeem010 <bigmuak010@gmail.com>
2025-04-20 21:02:23 +0100

    Refactor CSS styles in about.css for improved readability and consistency, including spacing adjustments and format standardization. Update carousel captions in about.html for enhanced expressiveness. Modify JavaScript in about.js to streamline button visibility logic and improve carousel functionality.

--- about me/about.js
+++ about me/about.js
@@ -1,20 +1,17 @@
-let lastScrollTop = 0; 
+let lastScrollTop = 0;
 const header = document.getElementById("header");
 
 window.addEventListener("scroll", function () {
-  let currentScroll = window.scrollY || document.documentElement.scrollTop; 
+  let currentScroll = window.scrollY || document.documentElement.scrollTop;
 
   if (currentScroll > lastScrollTop) {
-  
     header.classList.add("hidden");
   } else {
-  
     header.classList.remove("hidden");
   }
 
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 });
-
 
 const menuToggle = document.getElementById("menu-toggle");
 const navs = document.querySelector(".header-navs");
@@ -34,7 +31,6 @@ navLinks.forEach((link) => {
   });
 });
 
-
 const track = document.querySelector(".carousel-track");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
@@ -42,7 +38,7 @@ const carousel = document.querySelector(".carousel");
 const items = track.querySelectorAll(".carousel-item");
 
 let index = 0;
-let maxIndex = 0;
+let maxIndex = items.length;
 let imageWidth = 0;
 
 function getItemWidth() {
@@ -51,6 +47,11 @@ function getItemWidth() {
   const style = getComputedStyle(track);
   const gap = parseInt(style.columnGap || style.gap || 10);
   return item.getBoundingClientRect().width + gap;
+}
+
+function updateBtns() {
+  prevBtn.style.display = index ? "flex" : "none";
+  nextBtn.style.display = index < maxIndex ? "flex" : "none";
 }
 
 function calculateMaxIndex() {
@@ -72,15 +73,19 @@ prevBtn.addEventListener("click", () => {
   if (index > 0) {
     index--;
     updateTrackPosition();
+    updateBtns();
   }
 });
 
 nextBtn.addEventListener("click", () => {
   if (index < maxIndex) {
-    index++;
+    index++; // index = index + 1
     updateTrackPosition();
+    updateBtns();
   }
 });
 
 window.addEventListener("load", calculateMaxIndex);
 window.addEventListener("resize", calculateMaxIndex);
+
+updateBtns();
